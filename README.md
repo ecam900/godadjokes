589c99d9aeff65ab6ab2119f5ec554af72ee5278
Eric Crescioni <ecam900@gmail.com>
2021-07-05 00:12:15 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#### Dadjoke CLI is a tool that gives you a random dad joke.
+
+Built using:
+- Go
+- Cobra Framework (CLI Generator)
+- Icanhazdadjoke API
+
+Based on a tutorial by Div Rhino: https://www.youtube.com/watch?v=-tO7zSv80UY
---
+++ cmd/random.go
@@ -0,0 +1,87 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "Random provides a single random dad joke to the terminal.",
+	Run: func(cmd *cobra.Command, args []string) {
+		getRandomJoke()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+}
+
+type Joke struct {
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
+}
+
+func getRandomJoke() {
+	url := "https://icanhazdadjoke.com/"
+	responseBytes := getJokeData(url)
+	joke := Joke{}
+
+	if err := json.Unmarshal(responseBytes, &joke); err != nil {
+		log.Printf("Could not unmarshal response - %v", err)
+	}
+
+	fmt.Println(string(joke.Joke))
+
+}
+
+func getJokeData(baseAPI string) []byte {
+	request, err := http.NewRequest(
+		http.MethodGet,
+		baseAPI,
+		nil,
+	)
+
+	if err != nil {
+		log.Printf("Could not request a dadjoke - please try again 😥 - %v", err)
+	}
+
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("User-Agent", "Dadjoke CLI (tutorial)")
+
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		log.Printf("Could make a request!👊 - %v", err)
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Printf("Could not read response: %v", err)
+	}
+
+	return responseBytes
+}
